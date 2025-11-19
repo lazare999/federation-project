028bcb1f50d32c514fb46bf1aea979f14793b3a0
lazare999 <lazare.osiashvili9@gmail.com>
2025-11-19 19:27:59 +0400

    delete admin page

--- app/(admin)/admin/horses/page.js
+++
@@ -1,7 +0,0 @@
-import AddHorseForm from "@/admin-components/add-horse-form/addHorseForm";
-
-export default function AddHorses() {
-    return <div>
-        <AddHorseForm />
-    </div>
-}
--- app/(admin)/admin/sponsors/page.js
+++
@@ -1,7 +0,0 @@
-import AddSponsorsForm from "@/admin-components/add-sponsors-form/addSponsorsForms";
-
-export default function AddSponsors() {
-    return <div>
-        <AddSponsorsForm />
-    </div>
-}
